6dd9a379a60d632e8e638ea3089174d3e4ce644b
Jayesh Kawli <j.kawli@gmail.com>
2015-08-04 21:33:15 -0400

    Initial Commit

---
+++ JKFacebookPrideEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKFacebookPrideEffect
+//
+//  Created by Jayesh Kawli Backup on 8/4/15.
+//  Copyright (c) 2015 Jayesh Kawli Backup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
